1bb4ba726ffe7828a1a69c135bc4a95a0ee0397b
joaquimwalter <puyol.ufpa@gmail.com>
2025-10-18 22:38:07 -0300

    Pré lançamento go

---
+++ script.js
@@ -0,0 +1,25 @@
+let audio = new Audio('./src/musica dnj 2025.mp3');
+let isPlaying = false;
+
+
+document.querySelector('.play').addEventListener('click', function() {
+    if (!isPlaying) {
+        audio.loop = true;
+        audio.play();
+        isPlaying = true;
+    }
+});
+
+document.querySelector('.bi-pause-circle').addEventListener('click', function() {
+    if (isPlaying) {
+        audio.pause();
+        isPlaying = false;
+    }
+});
+
+document.querySelector('.bi-stop-circle').addEventListener('click', function() {
+    audio.pause();
+    audio.currentTime = 0;
+    isPlaying = false;
+});     
+
